bbb91e076434ca278525a589d9e9b92a3e44a665
kanbanfeed <kanbanfeed@alatreeventures.com>
2025-12-10 23:56:02 +0530

    updating

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/evaltree",
-  assetPrefix: "/evaltree",
+  /* config options here */
 };
-
 
 export default nextConfig;
