e4f6593cae4806fef5c35410880e8f2122687a36
kanbanfeed <kanbanfeed@alatreeventures.com>
2025-12-21 13:19:25 +0530

    updating

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,7 @@
 import type { NextConfig } from "next";
 
-const CROWBAR_ORIGIN =
-  process.env.CROWBAR_ORIGIN || "https://crowbarltd.com";
-
 const nextConfig: NextConfig = {
-  reactCompiler: false,
-
-  async redirects() {
-    return [
-      // redirect root
-      {
-        source: "/",
-        destination: `${CROWBAR_ORIGIN}/evaltree`,
-        permanent: true,
-      },
-      // redirect all paths
-      {
-        source: "/:path*",
-        destination: `${CROWBAR_ORIGIN}/evaltree/:path*`,
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
